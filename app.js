c0cf5818619df2ad5a360a63f06779b959cead64
Greg <mistzero0@gmail.com>
2023-11-08 11:54:06 +0800

    add app name

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.json({ success: true, app: "CI CD" });
+    res.json({ success: true, app: "CI CD Cpanel" });
 });
 
 app.listen(8080, () => {
