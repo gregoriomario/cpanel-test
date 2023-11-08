112423708cd67c166dd485a0c7a6e7d94c23e9d1
Greg <mistzero0@gmail.com>
2023-11-08 11:52:29 +0800

    add script

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.json({ success: true });
+    res.json({ success: true, app: "CI CD" });
 });
 
 app.listen(8080, () => {
