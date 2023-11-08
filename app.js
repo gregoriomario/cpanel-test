fe6004826848af089624db605e0ba5aa24da085b
Greg <mistzero0@gmail.com>
2023-11-08 13:18:47 +0800

    add message endpoint

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const app = express();
 app.get("/", (req, res) => {
     res.json({ success: true, app: "CI CD Cpanel" });
 });
+app.get("/message/:messsage", (req, res) => {
+    const message = req.params;
+    res.json({ message });
+});
 
 app.listen(8080, () => {
     console.log("Server is running");
